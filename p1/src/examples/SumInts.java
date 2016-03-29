8676e982ec44b3dea85693fa6bb335dd29fe792a
Nuno-Simoes <nunosimoes_86@hotmail.com>
2016-03-29 12:40:44 +0100

    changed examples from the source directory to their own package and included the expected outputs

--- p1/src/examples/SumInts.java
+++ p1/src/examples/SumInts.java
@@ -1,19 +1,20 @@
-public class SumIntegers {
-    private static long sumOfIntegerUptoN(Integer n) {
-        Long sum = 0L;
+package examples;
+public class SumInts {
+    private static long sumOfIntegerUptoN(int n) {
+        long sum = 0L;
         for (int i = 0; i < n; i++) {
             sum += i;
         }
         return sum;
     }
-    private static long printSum(Long n) {
-        //System.out.println("Sum: " + n);
+    private static long printSum(long n) {
+        System.out.println("Sum: " + n);
         return n;
     }
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         printSum(sumOfIntegerUptoN(100000000));
         long end = System.currentTimeMillis();
-        //System.out.println("Time: " + (end - start));
+        System.out.println("Time: " + (end - start));
     }
 }
